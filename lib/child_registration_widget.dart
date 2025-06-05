ba97ee200b443119e3dc19c9b49480b48b539306
JosR <josueramos17@hotmail.com>
2025-06-05 17:53:49 -0500

    First version of home and initial code of child registration

---
+++ lib/child_registration_widget.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ChildRegistration extends StatelessWidget {
+  const ChildRegistration({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Second Route')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context); // Go back to the previous screen 
+          },
+          child: const Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
